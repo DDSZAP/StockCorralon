89f0dff90a0073b06f99db9c3219b8d9932bb7f6
Ricardo Rojo <sistemas@zapala.gob.ar>
2024-08-12 13:48:21 -0300

    inicio

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './Components/NavBar/Navbar.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <h1>Sistema de Stock </h1>
+    </div>
+  );
+}
+
+export default App;
